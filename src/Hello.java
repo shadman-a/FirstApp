a2ff90ea143c6e3f0507a2af4fef240f125bf497
shadman-a <shadman@outlook.com>
2020-12-16 23:26:29 -0500

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        User obj = new User();
+        obj.setUserId(345);
+        System.out.println(obj.getUserId());
+    }
+}
---
+++ src/User.java
@@ -0,0 +1,11 @@
+public class User {
+    int UserId = 50;
+
+    public int getUserId() {
+        return UserId;
+    }
+
+    public void setUserId(int userId) {
+        UserId = userId;
+    }
+}
